6759cabf2cfd0cb19b22ef521b3a70181ac0204f
Chen <zdms1987@hotmail.com>
2019-07-19 16:50:36 -0500

    use a dedicated switch to toggle between different mode

--- ipython_profiles/profile_s6bm/startup/02-init.py
+++ ipython_profiles/profile_s6bm/startup/02-init.py
@@ -1,47 +1,63 @@
 print(f'Enter {__file__}...')
 # ----- generalized initialization ----- #
-keywords_func['init_tomo'] = '(Re)-initialized all devices with given mode'
-def init_tomo(mode='debug', config=None):
-    """
-    (Re)-initialize all devices based on given mode
+
+class RuntimeMode():
+
+    def __init__(self):
+        self._mode = 'debug'
+        self.set(mode='debug')
+
+    def __repr__(self):
+        return f"Current runtime mode is set to: {self._mode}"
+
+    def set(self, mode='debug', config=None):
+        """
+        (Re)-initialize all devices based on given mode
         simulated devices <-- debug
         actual devices    <-- dryrun, production
-    """
-    global A_shutter
-    global suspend_A_shutter
-    global tomostage
-    global preci, samX, ksamX, ksamZ, samY 
-    global psofly
-    global det
-
-    # re-init all tomo related devices
-    A_shutter = get_shutter(mode=mode)
-    tomostage = get_motors(mode=mode) 
-    preci = tomostage.preci              
-    samX = tomostage.samX               
-    ksamX = tomostage.ksamX
-    ksamZ = tomostage.ksamZ        
-    samY = tomostage.samY               
-    psofly = get_fly_motor(mode=mode)
-    det = get_detector(mode=mode)
-
-    # some quick sanity check production mode
-    import apstools.devices as APS_devices
-    aps = APS_devices.ApsMachineParametersDevice(name="APS")
-    if mode.lower() in ['production', 'dryrun']:
-        if aps.inUserOperations and (instrument_in_use() in (1, "6-BM-A")) and (not hutch_light_on()):
-            suspend_A_shutter = SuspendFloor(A_shutter.pss_state, 1)
-            RE.install_suspender(suspend_A_shutter)
+        """
+        if mode.lower() not in ['debug', 'dryrun', 'production']:
+            raise ValueError(f"Unknown mode: {mode}")
         else:
-            raise ValueError("Cannot be in production mode!")
-    else:
-        pass
+            self._mode = mode
+        
+        global A_shutter
+        global suspend_A_shutter
+        global tomostage
+        global preci, samX, ksamX, ksamZ, samY 
+        global psofly
+        global det
 
-    # TODO:
-    # initialize all values in the dictionary
+        # re-init all tomo related devices
+        A_shutter = get_shutter(mode=mode)
+        tomostage = get_motors(mode=mode) 
+        preci     = tomostage.preci              
+        samX      = tomostage.samX               
+        ksamX     = tomostage.ksamX
+        ksamZ     = tomostage.ksamZ        
+        samY      = tomostage.samY               
+        psofly    = get_fly_motor(mode=mode)
+        det       = get_detector(mode=mode)
+
+        # some quick sanity check production mode
+        import apstools.devices as APS_devices
+        aps = APS_devices.ApsMachineParametersDevice(name="APS")
+        if mode.lower() in ['production', 'dryrun']:
+            if aps.inUserOperations and (instrument_in_use() in (1, "6-BM-A")) and (not hutch_light_on()):
+                suspend_A_shutter = SuspendFloor(A_shutter.pss_state, 1)
+                RE.install_suspender(suspend_A_shutter)
+            else:
+                raise ValueError("Cannot be in production mode!")
+        else:
+            pass
+
+        # TODO:
+        # initialize all values in the dictionary
+
+mode = RuntimeMode()
 
 print(f"""
-🙊: Use init_tomo() to toggle between
+🙊: Use mode.set(mode=) to toggle between
     debug :     development mode, no hardware connection
     dryrun:     testing mode, connect to hardware, no beam
     production: ready for experiment
